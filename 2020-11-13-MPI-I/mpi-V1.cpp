a5047f4f9fb634109959d2c6e22b5c163e8a0257
Wind <wind@Wind>
2020-11-13 12:47:50 -0500

    .

--- 2020-11-13-MPI-I/mpi-V1.cpp
+++ 2020-11-13-MPI-I/mpi-V1.cpp
@@ -6,8 +6,12 @@
 int main(int argc, char **argv)
 {
   MPI_Init(&argc, &argv);
+  int pid=0; int nproc=0;
 
-  std::cout <<"Hello world"<<std::endl;;
+  MPI_Comm_rank(MPI_COMM_WORLD, & pid);
+  MPI_Comm_size(MPI_COMM_WORLD, & nproc);
+
+  std::cout <<"Hello world from "<<pid<<" out of "<<nproc<<std::endl;;
 
   MPI_Finalize();
   return 0;
